1c14fae6102660546be5d0ec9337007c6fae549e
wangke30 <wangke30@xiaomi.com>
2024-12-23 19:36:32 +0800

    [task] 食物卡片效果

--- MiHealth/app/src/main/java/com/hfut/mihealth/HomePage.kt
+++ MiHealth/app/src/main/java/com/hfut/mihealth/HomePage.kt
@@ -1,11 +1,81 @@
 package com.hfut.mihealth
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoginScreen(){
+fun HomePageScreen() {
+    var items = listOf("1", "2", "3")
+    LazyColumn {
+        items(items) { item ->
+            FoodCard()
+        }
+    }
+}
 
+@Preview
+@Composable
+fun FoodCard() {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        tonalElevation = 5.dp,
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+            .clickable { isExpanded = !isExpanded }
+    ) {
+        Row(
+            modifier = Modifier.padding(all = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "food picture",
+                modifier = if (!isExpanded) Modifier
+                    .size(60.dp)
+                    .clip(CircleShape) else Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(5.dp))
+            )
+            Spacer(Modifier.padding(horizontal = 8.dp))
+            Column {
+                Text(text = "食物名称")
+                Spacer(Modifier.padding(vertical = 8.dp))
+                Text(
+                    text = "介绍介绍介绍介绍介绍介绍介绍介绍绍介绍介绍介绍介绍介绍介绍绍介绍介绍介绍介绍介绍介绍绍介绍介绍介绍介绍介绍介绍绍介绍介绍介绍介绍介绍介绍",
+                    // 修改 maxLines 参数，在默认情况下，只显示一行文本内容
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    // Composable 大小的动画效果
+                    modifier = Modifier.animateContentSize()
+                )
+            }
+        }
+    }
 }
--- MiHealth/app/src/main/java/com/hfut/mihealth/MainActivity.kt
+++ MiHealth/app/src/main/java/com/hfut/mihealth/MainActivity.kt
@@ -6,10 +6,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemGestures
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -17,6 +25,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -44,6 +53,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun MainScreen() {
@@ -54,22 +64,29 @@ fun MainScreen() {
     )
     val navPos = rememberNavController()
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "首页") },
+                modifier = Modifier.windowInsetsTopHeight(WindowInsets.statusBars),
+                )
+        },
         bottomBar = {
             BottomNavigationBar(navItems,navPos)
         }
     ) {
         innerPadding ->
         print(innerPadding)
-
-        NavHost(navController = navPos , startDestination = "HomePage") {
-            composable("HomePage") {
-                AppContent("主页")
-            }
-            composable("jilu") {
-                AppContent("记录")
-            }
-            composable("mine") {
-                AppContent("我的")
+        Box(modifier = Modifier.padding(innerPadding)) {
+            NavHost(navController = navPos , startDestination = "HomePage") {
+                composable("HomePage") {
+                    HomePageScreen()
+                }
+                composable("jilu") {
+                    AppContent("记录")
+                }
+                composable("mine") {
+                    AppContent("我的")
+                }
             }
         }
     }
